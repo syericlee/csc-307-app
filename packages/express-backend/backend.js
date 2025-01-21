73cc81af804d5ea6bbd9fbcaf34bf428d084e0d9
syericlee <syeric1313@gmail.com>
2025-01-20 18:41:28 -0800

    Getting users by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -38,6 +38,9 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
 app.use(express.json());
 
 app.get("/users", (req, res) => {
@@ -48,6 +51,16 @@ app.get("/users", (req, res) => {
     res.send(result);
   } else {
     res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
   }
 });
 
