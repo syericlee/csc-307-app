08f11e58c3390c3e47dfa8a0d0aff32b435b0a23
syericlee <syeric1313@gmail.com>
2025-01-20 20:37:22 -0800

    Using the POST method

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -41,6 +41,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
   users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 app.use(express.json());
 
 app.get("/users", (req, res) => {
@@ -62,6 +67,12 @@ app.get("/users/:id", (req, res) => {
   } else {
     res.send(result);
   }
+});
+
+app.post("/users", (req, res) => {
+  const userToAdd = req.body;
+  addUser(userToAdd);
+  res.send();
 });
 
 app.listen(port, () => {
